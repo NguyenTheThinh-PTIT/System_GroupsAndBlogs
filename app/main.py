e64048c97ab1cacd2845706dd70b2f95c44b5ee4
TheThinh <thinhthenguyen2004@gmail.com>
2024-08-20 22:38:26 +0700

    login

--- app/main.py
+++ app/main.py
@@ -2,12 +2,12 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from .database import get_db, engine
 from . import models
-from .router import user
+from .router import user, auth
 
 models.Base.metadata.create_all(bind = engine)
 app = FastAPI()
 
 app.include_router(user.router)
-
+app.include_router(auth.router)
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -17,3 +17,11 @@ class UserOut(BaseModel):
 
     class Config:
         from_attributes = True
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
